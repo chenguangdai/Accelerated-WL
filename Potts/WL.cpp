a0d9229fc1600e7a9bf385ecf5f4639f3032dc8a
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-01-29 08:02:45 -0500

    Update WL.cpp

--- Potts/WL.cpp
+++ Potts/WL.cpp
@@ -15,11 +15,6 @@ int get_energy(const IntegerMatrix & state){
       if( j==(size-1) ) j1=0;
       else j1 = j+1;
       s += state(i,j) * (state(i,j1) + state(i1,j));
-      // int itop = (i+1) % size;
-      // int ibottom = ((i + size - 1) % size);
-      // int jright = (j+1) % size;
-      // int jleft = (j + size - 1) % size;
-      // s += state(i,j) * (state(itop, j) + state(ibottom, j) + state(i, jright) + state(i, jleft));
     }
   }
   return -s;
@@ -39,12 +34,14 @@ List MC_sweep(IntegerMatrix & state, NumericVector & logdensity, NumericVector &
   int itop, ibottom, jright, jleft;
   int current_spin, propose_spin, energy_old, energy_new, proposed_energy_level;
   double log_acceptance_prob;
+	
   for (int i = 0; i < size * size; i++){
     // Choose a random site for flipping
-	site = (int)(size * size * (runif(1))(0));
+    site = (int)(size * size * (runif(1))(0));
     si = site / size;
     sj = site % size;
-    // Calculate sum over neighbor spins 
+	  
+    // Calculate the sum over neighboring spins 
     itop = (si + 1) % size;
     ibottom = (si + size - 1) % size;
     jright = (sj + 1) % size;
@@ -53,9 +50,7 @@ List MC_sweep(IntegerMatrix & state, NumericVector & logdensity, NumericVector &
     current_spin = state(si, sj);
     energy_old = (current_spin == state(itop, sj)) + (current_spin == state(ibottom, sj)) + (current_spin == state(si, jright)) + (current_spin == state(si, jleft));
     energy_new = (propose_spin == state(itop, sj)) + (propose_spin == state(ibottom, sj)) + (propose_spin == state(si, jright)) + (propose_spin == state(si, jleft));
-    // Rcpp::Rcout << energy_old << std::endl;
-    // Rcpp::Rcout << energy_new << std::endl;
-    // Rcpp::Rcout << propose_spin << std::endl;
+
     // Calculate the energy of the proposed state
     proposed_energy_level = current_energy_level + energy_old - energy_new;
     // Calculate the acceptance probability
@@ -65,16 +60,12 @@ List MC_sweep(IntegerMatrix & state, NumericVector & logdensity, NumericVector &
     	state(si, sj) = propose_spin;
     	current_energy_level = proposed_energy_level;
     }
-    // Update logdensity, Histogram
-    // logdensity(current_energy_level) += (logf * 1.0 / pow(size, 2));
+	  
+    // Update the logdensity and the Histogram
     logdensity(current_energy_level) += logf;
-    // logdensity(current_energy_level) += -1.0 / (size * size - 1);
     Hist(current_energy_level)++;
-    // Normalize logdensity
-    // logdensity = logdensity - log(sum(exp(logdensity)) - 2);
-    // logdensity(1) = 0;
-    // logdensity(size * size - 1) = 0;
   }
+	
   // return
   return List::create(Named("lattice") = state, Named("energy_level") = current_energy_level, 
                       Named("logdensity") = logdensity, Named("Hist") = Hist);
