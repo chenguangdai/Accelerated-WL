be71ce30aa0713151a0c12bb0c782139a6004226
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-01-29 04:22:35 -0500

    Update get_specific_heat.R

--- Ising/get_specific_heat.R
+++ Ising/get_specific_heat.R
@@ -1,1 +1,11 @@
-
+### calculate the specific heat
+get_specific_heat <- function(logdensity, energy, temperature){
+  logw <- logdensity - energy/temperature
+  maxlw <- max(logw)
+  logw <- logw - maxlw
+  w <- exp(logw)/sum(exp(logw))
+  average_energy <- sum(energy * w)
+  average_squared_energy <- sum(energy^2 * w)
+  specific_heat <- (average_squared_energy - average_energy^2)/temperature^2
+  return(specific_heat)
+}
